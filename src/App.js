7229302e6c1d7907b2aa9c55ee04ffd341b4513f
TomekMichalski <tmm.michalski@gmail.com>
2019-08-06 21:23:05 +0200

    Implementing navbar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        <Route exact path="/rooms/:name" component={SingleRoom}/>
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
