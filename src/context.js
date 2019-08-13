ec8672bafcaccd546e11362e39cefb35e3a16d01
TomekMichalski <tmm.michalski@gmail.com>
2019-08-13 17:00:50 +0200

    Starting to work on the Room page

--- src/context.js
+++ src/context.js
@@ -8,11 +8,12 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true 
-  }
+    loading: true
+  };
   //getData
   componentDidMount() {
     let rooms = this.formatData(items);
+    
     let featuredRooms = rooms.filter(room => room.featured === true);
     this.setState({
       rooms,
@@ -20,23 +21,27 @@ class RoomProvider extends Component {
       sortedRooms: rooms,
       loading: false
     });
-    
   }
   formatData(items) {
     let tempItems = items.map(item => {
-      let id = item.sys.id; 
+      let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
-      let room = {...item.fields, images, id};
+      let room = { ...item.fields, images, id };
       return room;
-    })
+    });
     return tempItems;
   }
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
+    return room;
+  };
   render() {
     return (
-      <RoomContext.Provider value={{...this.state}}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
-    )
+    );
   }
 }
 
