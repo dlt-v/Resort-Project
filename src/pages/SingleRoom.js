6243a4c40d2e1c79d3423e11cee59ca25a5dc63e
TomekMichalski <tmm.michalski@gmail.com>
2019-08-14 11:37:07 +0200

    Implementing styled components, adding SingleRoom images section

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,42 +1,66 @@
-import React, { Component } from 'react';
-import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import { Link } from 'react-router-dom';
-import { RoomContext } from '../context';
+import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
+
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
     console.log(props);
-    
+
     this.state = {
       slug: this.props.match.params.slug,
       defaultBcg
-    }
+    };
   }
   static contextType = RoomContext;
   render() {
     const { getRoom } = this.context;
     const room = getRoom(this.state.slug);
-    
-    if(!room) {
-      return <div className="error">
-        <h3>No such room could be found...</h3>
-        <Link to="/rooms" className="btn-primary">
-          back to rooms
-        </Link>
-      </div>
-    }
-    const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
-    return (
-      <Hero hero="roomsHero">
-        <Banner title={`${name} room`}>
+
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room could be found...</h3>
           <Link to="/rooms" className="btn-primary">
-            Back to rooms
+            back to rooms
           </Link>
-        </Banner>
-      </Hero>
-    )
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+    const [mainImg, ...defaultImg] = images;
+    return (
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => (
+              <img src={item} key={index} alt={name} />
+            ))}
+          </div>
+        </section>
+      </>
+    );
   }
 }
+//3:17:50
