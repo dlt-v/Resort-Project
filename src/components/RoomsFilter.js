df3dc4ab7eb3a86fb7a5faa6b64f1720427d3129
TomekMichalski <tmm.michalski@gmail.com>
2019-08-15 12:31:53 +0200

    Implementing controled input structure

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "./Title";
+import { log } from "util";
+//get unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  console.log(context);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+  // ====================== Room types
+  let types = getUnique(rooms, "type");
+  types = ["all", ...types];
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
 
-export default function RoomsFilter() {
   return (
-    <div>
-      Room filter
-    </div>
-  )
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form action="" className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+      </form>
+    </section>
+  );
 }
--- src/context.js
+++ src/context.js
@@ -53,13 +53,34 @@ class RoomProvider extends Component {
     return room;
   };
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type, name, value);
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
   filterRooms = () => {
-    console.log("hello");
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+    let tempRooms = [...rooms];
+    if(type !== 'all'){
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    })
   };
   render() {
     return (
@@ -75,7 +96,6 @@ class RoomProvider extends Component {
     );
   }
 }
-//4:03:30
 const RoomConsumer = RoomContext.Consumer;
 
 export function withRoomConsumer(Component) {
